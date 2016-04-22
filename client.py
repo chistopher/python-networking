d4554a559a7c8edee2f4a4fb92c3bb2b54b0b2db
Christopher <christopher.weyand@student.hpi.de>
2016-04-22 22:53:37 +0200

    send mp3's through socket :)

--- client.py
+++ client.py
@@ -1,3 +1,61 @@
+# filechooser.py: simple file dialog for Android
+#
+# Copyright (c) dmych, 2014
+#
+# You may use, redistribute it and/or modify it
+# under the terms of the GNU General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License, or any later version.
+
+def _dialog(title, flist):
+    '''display dialog with list of files/folders title
+    allowing user to select any item or click Cancel
+    get user input and return selected index or None
+    '''
+    import androidhelper
+    droid = androidhelper.Android()
+    droid.dialogCreateAlert(title, '')
+    droid.dialogSetItems(flist)
+    droid.dialogSetNegativeButtonText('Cancel')
+    droid.dialogShow()
+    resp = droid.dialogGetResponse()
+    droid.dialogDismiss()
+    print resp
+    if resp.result.has_key('item'):
+        print 'RETURN:', resp.result['item']
+        return resp.result['item']
+    else:
+        return None
+
+def chooseFile(title='Choose File', folder='.'):
+    '''Display choose file dialog with title and a list of
+    files/folders in specified folder and allow user to
+    browse file system and select file.
+    Return full name of the file choosen or None
+    '''
+    import os.path
+    from glob import glob
+    d = folder
+    while True:
+        flist = [ os.path.split(fn)[1] for fn in glob(os.path.join(d, '*')) ]
+        if d != '/':               # if it is not root
+            flist.insert(0, '..')  # add parent
+        selected = _dialog(title, flist)
+        if selected is None:       # user cancelled
+            return None
+        d = os.path.abspath(os.path.join(d, flist[selected]))
+        if not os.path.isdir(d):
+            return d
+
+
+
 import socket
+import sys
+
+filepath = chooseFile(folder='./storage/emulated/0/Music')
+print "you chose" + filepath
+
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.2.107', 3378))
+with open(filepath, 'rb') as f:
+    s.sendall(f.read())
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 import vlc
-p = vlc.MediaPlayer('D:\Musik\Trap\TNGHT - Goooo.mp3')
+import sys
+
+#prepare player
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+#start connection
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', 3378))
 print "Socket has been bound"
@@ -8,7 +14,19 @@ serversocket.listen(1)
 while True:
     c, addr = serversocket.accept()     # Establish connection with client.
     print 'Got connection from', addr
-    c.close()
+    f = open('file_1.mp3','wb') #open in binary
+    l = c.recv(1024)
+    while (l):
+        f.write(l)
+        l = c.recv(1024)
+    f.close() #close file
+    c.close() #close connection socket
+
+    print 'received the file'
     print 'start playing'
-    p.stop() #stop the old playing
-    p.play()
+    media = instance.media_new('file_1.mp3')
+    player.stop()
+    player.set_media(media)
+    player.play()
+
+serversocket.close
