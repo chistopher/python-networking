99db23bce4dbae36b8a84d7ed1e2f671b4ac86fc
Christopher <christopher.weyand@student.hpi.de>
2016-04-22 19:02:06 +0200

    started music on pc with android phone

--- client.py
+++ client.py
@@ -1,3 +1,3 @@
 import socket
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.2.100', 3378))
+s.connect(('192.168.2.107', 3378))
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket
-
+import vlc
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', 3378))
 print "Socket has been bound"
 serversocket.listen(1)
-(clientsocket, address) = serversocket.accept()
-print "Connection has been accepted"
-    #ct = client_thread(clientsocket)
-    #ct.run()
+while True:
+   c, addr = serversocket.accept()     # Establish connection with client.
+   print 'Got connection from', addr
+   c.close()
+   p = vlc.MediaPlayer('D:\Musik\Trap\TNGHT - Goooo.mp3')
+   p.play()
