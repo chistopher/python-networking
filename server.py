6aeb17253c03c17fddfa1813d2e8fb71ccc95f96
Christopher <christopher.weyand@student.hpi.de>
2016-04-22 19:10:03 +0200

    prevent overlapping music

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import socket
 import vlc
+p = vlc.MediaPlayer('D:\Musik\Trap\TNGHT - Goooo.mp3')
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', 3378))
 print "Socket has been bound"
 serversocket.listen(1)
 while True:
-   c, addr = serversocket.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.close()
-   p = vlc.MediaPlayer('D:\Musik\Trap\TNGHT - Goooo.mp3')
-   p.play()
+    c, addr = serversocket.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    c.close()
+    print 'start playing'
+    p.stop() #stop the old playing
+    p.play()
